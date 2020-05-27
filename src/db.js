5433805bc46e37a542a28027d7341bfef3abe6e4
ABDELLAH JAIZE <crazy.igzo@gmail.com>
2020-05-27 01:33:25 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 const firebaseapp = firebase.initializeApp({
-  apiKey: "AIzaSyAbPEsNy8RnjzdHgMZvSemEiypx67lRR7E",
-  authDomain: "mr-jaize.firebaseapp.com",
-  databaseURL: "https://mr-jaize.firebaseio.com",
-  projectId: "mr-jaize",
-  storageBucket: "mr-jaize.appspot.com",
-  messagingSenderId: "940796553469",
-  appId: "1:940796553469:web:e787aee10c5f54e198b694",
-  measurementId: "G-WS4795D6S3"
+  apiKey: "API KEY HERE",
+  authDomain: "authDomain HERE",
+  databaseURL: "databaseURL HERE",
+  projectId: "projectId HERE",
+  storageBucket: "storageBucket HERE",
+  messagingSenderId: "messagingSenderId HERE",
+  appId: "appId HERE",
+  measurementId: "measurementId HERE"
 });
 
  export default firebaseapp.firestore();
